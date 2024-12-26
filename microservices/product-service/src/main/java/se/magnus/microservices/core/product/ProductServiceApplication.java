9729f44cbf37d8ad6c3a9462005d080a0b6a50e7
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2024-12-26 16:33:52 +0530

    Add implementation for all the microservices

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,11 @@ package se.magnus.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "se.magnus")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
---
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,24 @@
+package se.magnus.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.product.ProductService;
+import se.magnus.microservices.util.http.ServiceUtil;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+    private final ServiceUtil serviceUtil;
+
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
