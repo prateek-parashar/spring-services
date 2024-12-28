e753a7bcdef13f570c57476a9c6ec35573d7dad4
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2024-12-28 23:08:55 +0530

    Add event handling for all the core microservices

---
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/MessageProcessorConfig.java
@@ -0,0 +1,51 @@
+package se.magnus.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.product.ProductService;
+import se.magnus.api.event.Event;
+import se.magnus.api.exceptions.EventProcessingException;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class MessageProcessorConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageProcessorConfig.class);
+    private final ProductService productService;
+
+    @Autowired
+    public MessageProcessorConfig(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Bean
+    public Consumer<Event<Integer, Product>> messageProcessor() {
+        return event -> {
+            LOG.info("Message processor event received at: {}", event.getEventCreatedAt());
+
+            switch (event.getEventType()) {
+                case CREATE:
+                    Product product = event.getData();
+                    LOG.info("Creating product with id: {}", product.getProductId());
+                    productService.createProduct(product).block();
+                    break;
+                case DELETE:
+                    int productId = event.getKey();
+                    LOG.info("Deleting product with id: {}", productId);
+                    productService.deleteProduct(productId).block();
+                    break;
+                default:
+                    String errorMessage = "Unknown event type: " + event.getEventType();
+                    LOG.warn(errorMessage);
+                    throw new EventProcessingException(errorMessage);
+            }
+
+            LOG.info("Message processor completed at: {}", event.getEventCreatedAt());
+        };
+    }
+}
