21c346de5868cf89f120a643668707fdd2ff7b12
Prateek Parashar <36658243+prateek-parashar@users.noreply.github.com>
2024-12-30 22:51:45 +0530

    Update tests and config value for the username and password

--- spring-cloud/eureka-server/src/main/java/se/magnus/springcloud/eurekaserver/SecurityConfig.java
+++ spring-cloud/eureka-server/src/main/java/se/magnus/springcloud/eurekaserver/SecurityConfig.java
@@ -1,5 +1,6 @@
 package se.magnus.springcloud.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,36 +13,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    private final String username;
-    private final String password;
+  private final String username;
+  private final String password;
 
-    public SecurityConfig(
-            @Value("${app-eureka-username}")
-            String username,
-            @Value("${app-eureka-password}")
-            String password) {
-        this.username = username;
-        this.password = password;
-    }
+  @Autowired
+  public SecurityConfig(
+    @Value("${app.eureka-username}") String username,
+    @Value("${app.eureka-password}") String password
+  ) {
+    this.username = username;
+    this.password = password;
+  }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username(username)
-                .password(password)
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username(username)
+        .password(password)
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http
+      // Disable CRCF to allow services to register themselves with Eureka
+      .csrf()
+        .disable()
+      .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+    return http.build();
+  }
 }
--- spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -14,38 +14,38 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
-    @Value("${app.eureka-username}")
-    private String username;
+  @Value("${app.eureka-username}")
+  private String username;
 
-    @Value("${app.eureka-password}")
-    private String password;
+  @Value("${app.eureka-password}")
+  private String password;
 
-    @Autowired
-    void setTestRestTemplate(TestRestTemplate testRestTemplate) {
-        this.testRestTemplate = testRestTemplate.withBasicAuth(username, password);
-    }
+  @Autowired
+  void setTestRestTemplate(TestRestTemplate testRestTemplate) {
+    this.testRestTemplate = testRestTemplate.withBasicAuth(username, password);
+  }
 
-    private TestRestTemplate testRestTemplate;
+  private TestRestTemplate testRestTemplate;
 
-    @Test
-    void catalogLoads() {
+  @Test
+  void catalogLoads() {
 
-        String expectedReponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
-        ResponseEntity<String> entity = testRestTemplate.getForEntity("/eureka/apps", String.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals(expectedReponseBody, entity.getBody());
-    }
+    String expectedReponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
+    ResponseEntity<String> entity = testRestTemplate.getForEntity("/eureka/apps", String.class);
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
+    assertEquals(expectedReponseBody, entity.getBody());
+  }
 
-    @Test
-    void healthy() {
-        String expectedReponseBody = "{\"status\":\"UP\"}";
-        ResponseEntity<String> entity = testRestTemplate.getForEntity("/actuator/health", String.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals(expectedReponseBody, entity.getBody());
-    }
+  @Test
+  void healthy() {
+    String expectedReponseBody = "{\"status\":\"UP\"}";
+    ResponseEntity<String> entity = testRestTemplate.getForEntity("/actuator/health", String.class);
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
+    assertEquals(expectedReponseBody, entity.getBody());
+  }
 }
 
